69ad599a88f9a849f7ec4e53195d97ee1a9d1228
JHoo0118 <kik3078@gmail.com>
2024-07-12 23:37:40 +0900

    패키지 배포 및 설치

---
+++ vite.config.js
@@ -0,0 +1,37 @@
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+import { viteStaticCopy } from "vite-plugin-static-copy";
+export default defineConfig({
+    plugins: [
+        react(),
+        dts(),
+        viteStaticCopy({
+            targets: [
+                {
+                    src: "src/index.css",
+                    dest: "",
+                },
+            ],
+        }),
+    ],
+    build: {
+        lib: {
+            entry: path.resolve(__dirname, "src/index.tsx"),
+            fileName: function (format) { return "index.".concat(format, ".js"); },
+            name: "design-system-prac",
+        },
+        rollupOptions: {
+            external: ["react", "react-dom"],
+            output: {
+                globals: {
+                    react: "React",
+                    "react-dom": "ReactDOM",
+                },
+            },
+        },
+        sourcemap: true,
+        emptyOutDir: true,
+    },
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,38 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    dts(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "src/index.css",
+          dest: "",
+        },
+      ],
+    }),
+  ],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/index.tsx"),
+      fileName: (format) => `index.${format}.js`,
+      name: "design-system-prac",
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+    sourcemap: true,
+    emptyOutDir: true,
+  },
+});
